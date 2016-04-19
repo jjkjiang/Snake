8e03af2e4a124a4e36f95e6fb2bf386a61bedf4e
Jerry Jiang <jerryjiekaijiang@gmail.com>
2016-04-19 16:59:21 -0700

    Created test for snake movement, currently displays at correct area but no movement will investigate further.

--- GameObjects/Snake.cpp
+++ GameObjects/Snake.cpp
@@ -3,3 +3,26 @@
 //
 
 #include "Snake.h"
+
+Snake::Snake() {
+    size = 1;
+    head = new SnakeHead(sf::Vector2f(20 * pixelSizeX, 8 * pixelSizeY));
+    playerMove;
+}
+
+Snake::~Snake() {
+    delete head;
+}
+
+void Snake::updateSnake() {
+    playerMove.checkChangeDirection();
+    sf::Keyboard::Key dir = playerMove.getDirection();
+    if (dir == sf::Keyboard::Up) (head->shape).move(0, pixelSizeY);
+    if (dir == sf::Keyboard::Down) (head->shape).move(0, -pixelSizeY);
+    if (dir == sf::Keyboard::Left) (head->shape).move(-pixelSizeX, 0);
+    if (dir == sf::Keyboard::Right) (head->shape).move(pixelSizeX, 0);
+}
+void Snake::displaySnake(sf::RenderWindow& window) {
+    sf::RectangleShape curr = head->shape;
+    window.draw(curr);
+}
