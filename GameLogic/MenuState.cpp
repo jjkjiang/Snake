b5a863994eb8f1893fd38b7ad1d4fc0d1a7eba32
jerryjiekaijiang <jerryjiekaijiang@gmail.com>
2016-04-21 12:04:45 -0700

    Updates to MenuState and defined the appropriate tileset.

---
+++ GameLogic/MenuState.cpp
@@ -0,0 +1,21 @@
+#include "MenuState.h"
+#include "../Definitions/ResolutionVariables.h"
+#include "../Definitions/Logic.h"
+
+/*
+ * TileMap menuBg;
+    sf::RectangleShape snakeLogo;
+    sf::RectangleShape startSnakeButton;
+    //sf::RectangleShape startSnakePongButton;
+    //sf::RectangleShape optionsButton;
+    sf::RectangleShape exitButton;
+ */
+
+MenuState::MenuState() {
+    if (!menuBg.load(menuResPath, sf::Vector2u(pixelSizeX, pixelSizeY), level, xPixels, yPixels))
+        throw std::runtime_error("Failed to load the tilemap probably because file was not found.");
+    //snakeLogo;
+
+}
+void MenuState::update(const sf::Keyboard::Key&);
+void MenuState::drawState(sf::RenderWindow& window);
